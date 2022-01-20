98d397cb282cfcc325c9e145efc36651b13368e4
Matthew Yu <56417255+matthewyu01@users.noreply.github.com>
2022-01-19 20:44:23 -0800

    Add image upload & display

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,12 +17,13 @@ class ItemsController < ApplicationController
 
   # GET /items/1/edit
   def edit
+    @item.image.attach(params[:image])
   end
 
   # POST /items or /items.json
   def create
     @item = Item.new(item_params)
-
+    @item.image.attach(params[:image])
     respond_to do |format|
       if @item.save
         format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
@@ -65,6 +66,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :quantity, :price, :description)
+      params.require(:item).permit(:name, :quantity, :price, :description, :image)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   # Defines the root path route ("/")
-  root 'home#index'
+  root 'items#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'home/add'
 
 end
