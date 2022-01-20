8d7c2bf46b3ef376e105d5e55f9d29d7295cbfe9
Matthew Yu <56417255+matthewyu01@users.noreply.github.com>
2022-01-19 23:47:49 -1100

    Fix image display bug

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,6 @@ class ItemsController < ApplicationController
   # POST /items or /items.json
   def create
     @item = Item.new(item_params)
-    @item.image.attach(params[:image])
     respond_to do |format|
       if @item.save
         format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
